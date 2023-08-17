c84907670dfae9f8907cc7f5d261f21a7a246b91
Nbiba Bedis <bedisnbiba@gmail.com>
2023-08-17 20:11:27 +0100

    fmt

--- grammar.js
+++ grammar.js
@@ -4,7 +4,18 @@ module.exports = grammar({
   name: "treesitter_test",
   rules: {
     source_file: ($) => repeat($.case),
-    case: ($) => seq($.doubleBar, $._newLine, repeat1(seq($.caseName, $._newLine)), $.doubleBar, $._newLine, repeat1(seq($.code, $._newLine)), $.bar, $._newLine, $.expected),
+    case: ($) =>
+      seq(
+        $.doubleBar,
+        $._newLine,
+        repeat1(seq($.caseName, $._newLine)),
+        $.doubleBar,
+        $._newLine,
+        repeat1(seq($.code, $._newLine)),
+        $.bar,
+        $._newLine,
+        $.expected,
+      ),
     doubleBar: () => repeat1("="),
     _newLine: () => "\n",
     // don't allow casename that starts with =
